efe80fc15fb300b917947abf7e11ceba899076aa
Daulet Rakhmankul <daulet.rakhmankul@gmail.com>
2020-01-06 17:54:02 +0600

    Testti ashatyn stranitsa qosyldy

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import os
 import json
+from time import time
 
-from bottle import static_file, view, run, route, template, request, redirect
+from bottle import run, route, static_file, template, view, request, redirect
 
 from utilities import get_documents
 from foxtest.Converter import *
 from foxtest.QuizMap import *
+from foxtest.Subject import *
 
 
 @route('/static/<filename:path>')
@@ -26,8 +28,12 @@ def home():
 def document(folder, name):
     dir = 'docs/single/' if folder == 's' else 'docs/multiple/'
     abs_dir = os.path.abspath(dir)
-    map_json_path = os.path.join(abs_dir, name + '.map.json')
+    # if quiz converted then redirect to /quiz/name.json
+    quiz_json_path = os.path.join(abs_dir, name + '.json')
+    if os.path.exists(quiz_json_path):
+        return redirect('/' + folder + '/' + name + '.json')
 
+    map_json_path = os.path.join(abs_dir, name + '.map.json')
     info = None
     if os.path.exists(map_json_path):
         with open(map_json_path, 'r') as file:
@@ -35,6 +41,18 @@ def document(folder, name):
         info = quiz_map.info
 
     return template('converter.html', {'name': name, 'folder': folder, 'info': info})
+
+
+@route('/<folder:re:[ms]>/<name>.json')
+def quiz(folder, name):
+    dir = 'docs/single/' if folder == 's' else 'docs/multiple'
+    abs_dir = os.path.abspath(dir)
+    quiz_json_path = os.path.join(abs_dir, name + '.json')
+    if not os.path.exists(quiz_json_path):
+        return f'Ошибка. Тест с названием {name}.json не найден'
+    with open(quiz_json_path, 'r', encoding='utf-8') as file:
+        subject = json.load(file, object_hook=decode_subject)
+    return template('quiz.html', {'name': name, 'subject': subject.get_dict()})
 
 
 @route('/find/<folder:re:[ms]>/<name>.doc', method='POST')
@@ -59,13 +77,21 @@ def convert_(folder, name):
     abs_dir = os.path.abspath(dir)
     document_path = os.path.join(abs_dir, name + '.doc')
     map_json_path = os.path.join(abs_dir, name + '.map.json')
+    quiz_json_path = os.path.join(abs_dir, name + '.json')
+    mode = 1 if folder == 's' else 2
     if not os.path.exists(document_path):
         return f'Ошибка: документ не найден. Путь: {document_path}'
+    if not os.path.exists(map_json_path):
+        return f'Ошибка: карта документа не найден. Путь: {map_json_path}'
     with open(map_json_path, 'r') as file:
         quiz_map = json.load(file, object_hook=decode_quiz_map)
     word, doc = open_document(document_path)
     quizes = convert(doc, quiz_map)
-    return {'quizes': quizes}
+
+    subject = Subject.create(mode=mode, quizes=quizes)
+    with open(quiz_json_path, 'w') as file:
+        json.dump(subject, file, cls=SubjectEncoder)
+    return redirect('/'+folder+'/'+name+'.doc')
 
 
 run(host='localhost', port=8080, debug=True, reloader=True)
--- foxtest/Subject.py
+++ foxtest/Subject.py
@@ -13,14 +13,15 @@ class Subject:
     quizes: list = None
 
     @staticmethod
-    def create(name: str, code: str, mode: int, comment: str, quizes: list) -> 'Subject':
+    def create(_v: float = 0.2, name: str = '', code: str = '', mode: int = 0, created: float = None, comment: str = '', quizes: list = None) -> 'Subject':
         subject = Subject()
+        subject._v = _v
         subject.name = name
         subject.code = code
         subject.mode = mode
-        subject.created = time.time()
+        subject.created = created or time.time()
         subject.comment = comment
-        subject.quizes = quizes
+        subject.quizes = quizes or []
         return subject
 
     def get_dict(self):
@@ -38,3 +39,25 @@ class Subject:
         jpath = os.path.join(os.path.abspath(''), self.code + '.json')
         with open(jpath, 'w') as jout:
             json.dump(self.get_dict(), jout)
+
+
+class SubjectEncoder(json.JSONEncoder):
+    def default(self, obj: Subject):
+        if isinstance(obj, Subject):
+            quiz = {
+                '_v': obj._v,
+                'name': obj.name,
+                'code': obj.code,
+                'mode': obj.mode,
+                'created': obj.created,
+                'comment': obj.comment,
+                'quizes': obj.quizes
+            }
+            return quiz
+
+
+def decode_subject(dic):
+    if dic.get('_v') == 0.2 and dic.get('quizes'):
+        return Subject.create(**dic)
+    else:
+        return dic
